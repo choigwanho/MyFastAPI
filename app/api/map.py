47ba0514b5fd4f83642ad89502aafd749b790e76
dev-choee <rhksgh99@gmail.com>
2022-08-10 21:09:48 +0900

    지도 거리 연산 기능 테스트 완료

--- app/api/map.py
+++ app/api/map.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.responses import JSONResponse
+from pydantic import BaseModel
 
 templates = Jinja2Templates(directory="templates/")
 
 router = APIRouter()
 
+class Spot(BaseModel):
+    lat: str
+    lng: str
+
 @router.get("/main", response_class=HTMLResponse)
 async def get_map(request: Request):
     return templates.TemplateResponse("map_main.html",{"request": request})
+
+@router.post("/search", response_class=HTMLResponse)
+async def get_map(spot: Spot):
+    dicted_spot = dict(spot)
+    dicted_spot['success'] = True
+
+    return JSONResponse(dicted_spot)
